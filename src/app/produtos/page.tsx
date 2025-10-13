4aae4f326e371eb642f78b2942ef733ccf671f33
Matheus Batista <matheusifro2020@gmail.com>
2025-10-13 18:28:28 -0400

    fix: carregando produtos automaticamente

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 import { TypographyH2 } from "@/components/layout/subtitle";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TabelaProdutos from "@/components/layout/table/produtoTable";
 import { Produto } from "@/components/layout/table/produtoTable";
 
@@ -18,7 +18,8 @@ export default function ProdutosPage() {
       const accessToken =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4ZGMyZTk3OGE0NWIwMmM2ZDA0N2JmOCIsImlhdCI6MTc2MDMxMzYxNywiZXhwIjoxNzYwNDAwMDE3fQ.npotO-694DrgQ3S0Sbe8Vb6y9CEVvCLOFZk2kZv4uCA";
 
-      const response = await fetch("http://localhost:5011/produtos", {
+        // TODO:carregar automaticamente 20 produtos ficará mais harmônico na tabela
+      const response = await fetch("http://localhost:5011/produtos/?limite=20", {
         method: "GET",
         headers: {
           Authorization: `Bearer ${accessToken}`,
@@ -39,16 +40,16 @@ export default function ProdutosPage() {
     }
   }
 
+  useEffect(() => {
+    fetchProdutos();
+  }, []);
+
   return (
     <div>
       <Header />
 
       <main className="min-h-screen p-8">
         <TypographyH2>Estoque de produtos</TypographyH2>
-
-        <Button onClick={fetchProdutos} className="mb-6">
-          Buscar Produtos na API
-        </Button>
 
         <TabelaProdutos produtos={produtos} />
       </main>
--- src/components/layout/subtitle/index.tsx
+++ src/components/layout/subtitle/index.tsx
@@ -1,6 +1,6 @@
 export function TypographyH2({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 pb-10">
       <h2 className="scroll-m-20 mb-2 text-2xl font-semibold tracking-tight first:mt-0">
         {children}
       </h2>
