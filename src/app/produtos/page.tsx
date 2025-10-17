c0e42e0555b6e21485fd69a11459f5edbfc6cbd4
Matheus Batista <matheusifro2020@gmail.com>
2025-10-17 17:28:25 -0400

    feat: adicionando loading icon

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import Footer from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 import { TypographyH2 } from "@/components/layout/subtitle";
-import { useState, useEffect, use } from "react";
 import TabelaProdutos from "@/components/layout/table/produtoTable";
 import { Produto } from "../../lib/Produto";
 import { useQuery } from "@tanstack/react-query";
 import { fetchData } from "@/services/api";
+import { LoaderIcon } from "lucide-react";
+import { toast } from "sonner";
+import { useEffect } from "react";
 
 export default function ProdutosPage() {
   const {
@@ -19,6 +20,10 @@ export default function ProdutosPage() {
   } = useQuery({
     queryKey: ["listaProdutos"],
     queryFn: async () => {
+      if (process.env.NEXT_PUBLIC_SIMULAR_ERRO === "true") {
+        throw new Error("Erro simulado ao carregar dados");
+      }
+
       const result = await fetchData<{ data: { docs: Produto[] } }>(
         "/produtos?limite=20",
         "GET"
@@ -34,13 +39,14 @@ export default function ProdutosPage() {
       <main className="min-h-screen p-8">
         <TypographyH2>Estoque de produtos</TypographyH2>
 
-        {produtosIsLoading && <p>Carregando produtos...</p>}
+        {produtosIsLoading && (
+          <LoaderIcon role="status" className="animate-spin mt-20 mx-auto" />
+        )}
 
         {produtosIsError && (
-          <p className="text-red-500">
-            Erro ao carregar produtos:{" "}
-            {(produtosError as Error)?.message || "Erro desconhecido"}
-          </p>
+          toast.error("Erro ao carregar produtos", {
+            description: (produtosError as Error)?.message || "Erro desconhecido",
+          })
         )}
 
         {produtosData && <TabelaProdutos produtos={produtosData} />}
